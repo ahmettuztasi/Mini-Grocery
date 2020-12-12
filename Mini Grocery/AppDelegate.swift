a539d95402dda1efdfb094c2f7eb4798767af142
ahmettuztasi <ahmetuztasi@gmail.com>
2020-12-12 19:48:15 +0300

    project created

---
+++ Mini Grocery/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Mini Grocery
+//
+//  Created by ahmet on 12.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
